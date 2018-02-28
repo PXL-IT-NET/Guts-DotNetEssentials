ef96ea0f171a70160f4356a62785a7e8150f5c92
Wesley Hendrikx <wesleyhendrikx@hotmail.com>
2018-02-28 08:47:55 +0100

    fix #5: Chapter5, exercise 5: allow use of System.Windows.Point

--- Chapter5/Exercise05.Tests/MainWindowTests.cs
+++ Chapter5/Exercise05.Tests/MainWindowTests.cs
@@ -43,9 +43,12 @@ namespace Exercise05.Tests
             var sourceCode = Solution.Current.GetFileContent(@"Exercise05\MainWindow.xaml.cs");
             sourceCode = CodeCleaner.StripComments(sourceCode);
 
-            Assert.That(sourceCode, Contains.Substring("new Polygon();"), () => "No code found where an instance of the Polygon class is created.");
-            Assert.That(sourceCode, Contains.Substring("new Point("), () => "No code found where instances op the Point class are created.");
-            Assert.That(sourceCode, Contains.Substring(".Add("), () => "No code found where points are added to the Polygon instance.");
+            Assert.That(sourceCode, Contains.Substring("new Polygon();"), 
+                () => "No code found where an instance of the Polygon class is created.");
+            Assert.That(sourceCode, Contains.Substring("new Point(").Or.Contains("new System.Windows.Point("), 
+                () => "No code found where instances op the Point class are created.");
+            Assert.That(sourceCode, Contains.Substring(".Add("), 
+                () => "No code found where points are added to the Polygon instance.");
         }
 
         [MonitoredTest("Should draw triangles and rectangles on the canvas after the draw button is clicked"), Order(3)]
