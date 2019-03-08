ceaf662af2ec1ae6514fe3d3e4e3eafa8125e82d
Kris Hermans <kris.hermans@live.be>
2019-03-08 20:46:49 +0100

    test if Beetle class exists

--- Chapter10/ExerciseBeetle.Tests/BeetleTests.cs
+++ Chapter10/ExerciseBeetle.Tests/BeetleTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ExerciseBeetle.Tests
 {
@@ -15,23 +16,38 @@ namespace ExerciseBeetle.Tests
     public class BeetleTests
     {
         private object _beetleObject = null;
-        
+        private readonly string _beetleType = "ExerciseBeetle.Beetle";
+        private readonly string _beetleAssembly = "ExerciseBeetle";
+        private readonly int _beetleSize = 10;
+        private readonly int _beetleX = 40;
+        private readonly int _beetleY = 35;
+
+        private Canvas _testCanvas;
         // Todo
-        // - test if Beetle class exists
         // - test all members of Beetle
 
         [SetUp]
         public void Setup()
-        { }
+        {
+            _testCanvas = new Canvas
+            {
+                Width = 536, // mimic the same size as in the starter file
+                Height = 356
+            };
+            
+            object[] parameters = new object[] { _testCanvas, _beetleSize, _beetleX, _beetleY };
+            Type type = Type.GetType($"{_beetleType}, {_beetleAssembly}");
+            _beetleObject = Activator.CreateInstance(type, parameters);
+        }
 
         [TearDown]
         public void TearDown()
         { }
 
-        [MonitoredTest("There should be a class named Beetle"), Order(1)]
+        [MonitoredTest("Beetle - There should be a class named Beetle"), Order(1)]
         public void _1_ShouldHaveAClassNamedBeetle()
         {
-            Assert.Fail();
+            Assert.That(_beetleObject, Is.Not.Null, $"Could not create an instance of class {_beetleType}");
         }
     }
 }
