315db0bc979540eb009f00bb65d8afca3107f046
Kris Hermans <kris.hermans@live.be>
2019-03-08 17:35:02 +0100

    test setup

---
+++ Chapter10/ExerciseBeetle.Tests/BeetleTests.cs
@@ -0,0 +1,37 @@
+﻿using Guts.Client.Classic;
+using Guts.Client.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExerciseBeetle.Tests
+{
+    [ExerciseTestFixture("dotNet1", "H10", "ExerciseBeetle", @"ExerciseBeetle\Beetle.cs"),
+     Apartment(ApartmentState.STA)]
+    public class BeetleTests
+    {
+        private object _beetleObject = null;
+        
+        // Todo
+        // - test if Beetle class exists
+        // - test all members of Beetle
+
+        [SetUp]
+        public void Setup()
+        { }
+
+        [TearDown]
+        public void TearDown()
+        { }
+
+        [MonitoredTest("There should be a class named Beetle"), Order(1)]
+        public void _1_ShouldHaveAClassNamedBeetle()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Chapter10/ExerciseBeetle.Tests/MainWindowTests.cs
@@ -0,0 +1,34 @@
+﻿using Guts.Client.Classic;
+using Guts.Client.Shared;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExerciseBeetle.Tests
+{
+    [ExerciseTestFixture("dotNet1", "H10", "ExerciseBeetle", @"ExerciseBeetle\MainWindow.xaml;ExerciseBeetle\MainWindow.xaml.cs"),
+     Apartment(ApartmentState.STA)]
+    public class MainWindowTests
+    {
+        // Todo
+        // Test Window functionality
+
+        [SetUp]
+        public void Setup()
+        { }
+
+        [TearDown]
+        public void TearDown()
+        { }
+
+        [MonitoredTest("Should have a private member of class Beetle"), Order(1)]
+        public void _1_ShouldHaveAPrivateBeetleMember()
+        {
+            Assert.Fail();
+        }
+    }
+}
